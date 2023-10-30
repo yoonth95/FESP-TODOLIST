48a0c24611d798930fa6e54b6aa9f38a18dd3483
Suam Kang <ksa199653@gmail.com>
2023-10-30 22:32:05 +0900

    fix: 텍스트얼라인 센터로 수정

--- 01.javascript/todoapp-router/assets/js/layout/Header.js
+++ 01.javascript/todoapp-router/assets/js/layout/Header.js
@@ -1,6 +1,7 @@
-const Header = function(title){
-  const headerNode = document.createElement('header');
-  const h1 = document.createElement('h1');
+const Header = function (title) {
+  const headerNode = document.createElement("header");
+  headerNode.style.textAlign = "center";
+  const h1 = document.createElement("h1");
   const headerTitle = document.createTextNode(title);
   h1.appendChild(headerTitle);
   headerNode.appendChild(h1);
