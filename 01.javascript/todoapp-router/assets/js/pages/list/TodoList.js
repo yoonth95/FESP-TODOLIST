4472540536004e1c658f0a43a12174b3c145443c
hungerbk <wjantlr@gmail.com>
2023-10-30 16:14:57 +0900

    Setting: todoapp에서 todoapp-router로 파일 이동

--- 01.javascript/todoapp-router/assets/js/pages/list/TodoList.js
+++ 01.javascript/todoapp-router/assets/js/pages/list/TodoList.js
@@ -1,54 +1,77 @@
 // 할일 목록
-import Header from '../../layout/Header.js';
-import Footer from '../../layout/Footer.js';
-import {linkTo} from '../../Router.js';
+import Header from "../../layout/Header.js";
+import Footer from "../../layout/Footer.js";
+import { linkTo } from "../../Router.js";
 
-const TodoList = async function(){
-  const page = document.createElement('div');
-  page.setAttribute('id', 'page');
+const TodoList = async function () {
+  const page = document.createElement("div");
+  page.setAttribute("id", "page");
 
-  const content = document.createElement('div');
-  content.setAttribute('id', 'content');
+  const contents = document.createElement("div");
+  contents.setAttribute("id", "contents");
+  contents.setAttribute("class", "todo-container");
   let response;
-  try{    
-    response = await axios('http://localhost:33088/api/todolist');
+  try {
+    response = await axios("http://localhost:33088/api/todolist");
 
-    const ul = document.createElement('ul');
-    ul.setAttribute('class', 'todolist');
-    response.data?.items.forEach(item => {
-      const li = document.createElement('li');
-      const todoInfoLink = document.createElement('a');
-      todoInfoLink.setAttribute('href', `info?_id=${item._id}`);
+    const ul = document.createElement("ul");
+    ul.setAttribute("class", "todolist");
+    response.data?.items.forEach((item) => {
+      /* todoItem 초기렌더링 */
+      const li = document.createElement("li");
+      li.setAttribute("class", "todolist__item");
+
+      /* todoItem 중요버튼 */
+      const importantButton = document.createElement("button");
+      importantButton.setAttribute("id", "importantButton");
+
+      /* todoItem 삭제 버튼 */
+      const deleteButton = document.createElement("a");
+      deleteButton.setAttribute("id", "deleteButton");
+      deleteButton.setAttribute("href", `_id=${item._id}`);
+
+      /* todoItem 상세보기 모달링크 */
+      const todoInfoLink = document.createElement("a");
+      todoInfoLink.setAttribute("href", `info?_id=${item._id}`);
+      todoInfoLink.setAttribute("class", "todolist__item--todo-info-link");
       const title = document.createTextNode(item.title);
 
-      todoInfoLink.addEventListener('click', function(event){
+      /* todoItem 체크박스 */
+      const checkbox = document.createElement("input");
+      checkbox.setAttribute("type", "checkbox");
+      checkbox.setAttribute("class", "todolist__item--checkbox");
+      // checkbox.setAttribute("name", `${item.name}`);
+
+      todoInfoLink.addEventListener("click", function (event) {
         // 브라우저의 기본 동작 취소(<a> 태그 동작 안하도록)
         event.preventDefault();
-        linkTo(todoInfoLink.getAttribute('href'));
+        linkTo(todoInfoLink.getAttribute("href"));
       });
 
       todoInfoLink.appendChild(title);
+      li.appendChild(checkbox);
       li.appendChild(todoInfoLink);
+      li.appendChild(importantButton);
+      li.appendChild(deleteButton);
       ul.appendChild(li);
     });
-    content.appendChild(ul);    
+    contents.appendChild(ul);
 
-    const btnRegist = document.createElement('button');
-    const btnTitle = document.createTextNode('등록');
+    const btnRegist = document.createElement("button");
+    const btnTitle = document.createTextNode("등록");
     btnRegist.appendChild(btnTitle);
-    content.appendChild(btnRegist);
+    contents.appendChild(btnRegist);
 
-    btnRegist.addEventListener('click', () => {
-      linkTo('regist');
+    btnRegist.addEventListener("click", () => {
+      linkTo("regist");
     });
-
-  }catch(err){
-    const error = document.createTextNode('일시적인 오류 발생');
-    content.appendChild(error);
+  } catch (err) {
+    const error = document.createTextNode("일시적인 오류 발생");
+    contents.appendChild(error);
   }
-  
-  page.appendChild(Header('TODO App 목록 조회'));
-  page.appendChild(content);
+
+  page.appendChild(Header("TODO App 목록 조회"));
+  page.appendChild(contents);
   page.appendChild(Footer());
   return page;
 };
