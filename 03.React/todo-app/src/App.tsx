86861c1522e00b2f44045144463badeb7947b791
Suam Kang <ksa199653@gmail.com>
2023-11-10 13:33:23 +0900

    chore: tsx파일 추가 밑 환경설정

--- 03.React/todo-app/src/App.tsx
+++ 03.React/todo-app/src/App.tsx
@@ -1,7 +1,37 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
+import TodoList from "@/pages/list/TodoList";
+import TodoRegist from "@/pages/regist/TodoRegist";
+import TodoInfo from "@/pages/info/TodoInfo";
+import TodoUpdate from "@/pages/update/TodoUpdate";
+import Error from "@/pages/errors/Error";
+
+// 라우터 설정
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <TodoList />,
+  },
+  {
+    path: "/regist",
+    element: <TodoRegist />,
+  },
+  {
+    path: "/info",
+    element: <TodoInfo />,
+  },
+  {
+    path: "/edit",
+    element: <TodoUpdate />,
+  },
+  {
+    path: "/*",
+    element: <Error />,
+  },
+]);
 
 function App() {
-  return <></>;
+  return <RouterProvider rotuer={router} />;
 }
 
 export default App;
---
+++ 03.React/todo-app/src/pages/update/TodoUpdate.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TodoUpdate = () => {
+  return <div></div>;
+};
+
+export default TodoUpdate;
