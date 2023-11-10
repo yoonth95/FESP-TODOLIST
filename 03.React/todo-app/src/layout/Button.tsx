76546b5a7a8eee6f1e798c38802a71c402981ec2
윤태현 <yoonth0919@gmail.com>
2023-11-10 15:32:41 +0900

    Fix: Button.tsx 타입 부분 reset 타입 삭제

--- 03.React/todo-app/src/layout/Button.tsx
+++ 03.React/todo-app/src/layout/Button.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 interface ButtonProps {
   className: string;
-  type: "button" | "submit" | "reset";
+  type: "button" | "submit";
   text: string;
   handleClick?: () => void;
 }
