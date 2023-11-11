0635bf7dc696a501281f29f7fc16500bf420ec01
Suam Kang <ksa199653@gmail.com>
2023-11-11 18:41:04 +0900

    feat: 전체완료, 전체삭제 기능 수정

--- 03.React/todo-app/src/pages/list/TodoList.tsx
+++ 03.React/todo-app/src/pages/list/TodoList.tsx
@@ -6,7 +6,7 @@ import BaseUrl from "../../api/BaseUrl";
 import TodoListItem from "./TodoListItem";
 import useDataFilter from "../../hooks/useDataFilter";
 
-import "./TodoList.css"
+import "./TodoList.css";
 
 const TodoList: React.FC = () => {
   // const { _id } = useParams();
@@ -14,8 +14,8 @@ const TodoList: React.FC = () => {
   const { filterData } = useDataFilter([]); // 필터링 커스텀 훅
 
   const [allTodos, setAllTodos] = useState<TodoList>([]);
-  const [todos, setTodos] = useState<TodoList>([]);
   const [importantTodos, setImportantTodos] = useState<TodoList>([]);
+  const [todos, setTodos] = useState<TodoList>([]);
 
   // get 요청
   const fetchData = async () => {
@@ -28,7 +28,7 @@ const TodoList: React.FC = () => {
       const newTodos: TodoItem[] = [];
       todosResponse.forEach((todo: TodoItem) => {
         todo.important ? newImportantTodos.push(todo) : newTodos.push(todo);
-      })
+      });
 
       // 상태를 한 번에 업데이트
       setAllTodos(todosResponse);
@@ -74,28 +74,39 @@ const TodoList: React.FC = () => {
 
     if (deleteResult) {
       if (important) {
-        const filterImportantTodo = importantTodos.filter(todo => todo._id !== _id);
+        const filterImportantTodo = importantTodos.filter(
+          (todo) => todo._id !== _id
+        );
         setImportantTodos([...filterImportantTodo]);
       } else {
-        const filterTodo = todos.filter(todo => todo._id !== _id);
+        const filterTodo = todos.filter((todo) => todo._id !== _id);
         setTodos([...filterTodo]);
       }
 
       await axios.delete(`${BaseUrl}/${_id}`);
     }
-  }
+  };
 
   // 중요 체크
-  const importantItem = async (btn: React.MouseEvent<HTMLButtonElement>, _id: number, important: boolean) => {
+  const importantItem = async (
+    btn: React.MouseEvent<HTMLButtonElement>,
+    _id: number,
+    important: boolean
+  ) => {
     if (important) {
       btn.currentTarget.classList.remove("fill");
       // 중요 항목에서 뺀 것
-      const filterImportantTodos = importantTodos.filter(todo => todo._id !== _id);
+      const filterImportantTodos = importantTodos.filter(
+        (todo) => todo._id !== _id
+      );
 
       // 뺀 항목을 중요하지 않는 항목에 추가
-      const popImportantTodo = importantTodos.find(todo => todo._id === _id);
+      const popImportantTodo = importantTodos.find((todo) => todo._id === _id);
       if (popImportantTodo) {
-        const updateTodos = [...todos, {...popImportantTodo, important: false}];
+        const updateTodos = [
+          ...todos,
+          { ...popImportantTodo, important: false },
+        ];
         setTodos([...updateTodos]);
         setImportantTodos([...filterImportantTodos]);
         await axios.patch(`${BaseUrl}/${_id}`, {
@@ -105,12 +116,15 @@ const TodoList: React.FC = () => {
     } else {
       btn.currentTarget.classList.add("fill");
       // 중요하지 않은 항목에서 뺀 것
-      const filterTodos = todos.filter(todo => todo._id !== _id);
+      const filterTodos = todos.filter((todo) => todo._id !== _id);
 
       // 뺀 항목을 중요 항목에 추가
-      const popTodo = todos.find(todo => todo._id === _id);
+      const popTodo = todos.find((todo) => todo._id === _id);
       if (popTodo) {
-        const updateImportantTodos = [...importantTodos, {...popTodo, important: true}];
+        const updateImportantTodos = [
+          ...importantTodos,
+          { ...popTodo, important: true },
+        ];
         setImportantTodos([...updateImportantTodos]);
         setTodos([...filterTodos]);
         await axios.patch(`${BaseUrl}/${_id}`, {
@@ -118,7 +132,7 @@ const TodoList: React.FC = () => {
         });
       }
     }
-  }
+  };
 
   // 미완료, 완료, 중요 탭 필터버튼 함수
   const dataFilterHandler = (todosData: TodoList, value: string) => {
@@ -132,23 +146,50 @@ const TodoList: React.FC = () => {
 
   // 전체 완료 함수
   const allDoneHandler = async () => {
-    // 통신해서 서버에 저장하는 for문 돌리기 -> promise all
+    // 통신해서 서버에 저장하는 for문 돌리기 -> Promise.allSettled
     try {
-      await Promise.allSettled(
+      const allCheckRequset = await Promise.allSettled(
         allTodos.map((item) =>
           axios.patch(`${BaseUrl}/${item._id}`, { done: !item.done })
         )
       );
-    } catch (error) {
-      console.error("Error updating todos:", error);
-    }
 
-    // 화면에 보여지는 애들 for문 돌리기
-    // todos, importantTodo => done 전부 !done
+      // 화면에 보여지는 애들 for문 돌리기
+      // todos, importantTodo => done 전부 !done 업데이트하기
+      const updatedTodos = allTodos.map((todo, idx) => {
+        if (allCheckRequset[idx].status === "fulfilled") {
+          // patch요청 성공하면 done 값 토글
+          return { ...todo, done: !todo.done };
+        } else {
+          return todo;
+        }
+      });
+
+      setAllTodos([...updatedTodos]); // 렌더링 이슈 : 서버요청은 정상, 렌더링은 새로고침해야 적용됨 -> 리렌더 이슈
+    } catch (error) {
+      console.error("항목을 모두 완료시키는데 요청 오류 발생", error);
+    }
   };
 
   // 전체 삭제 함수
-  const allDeleteHandler = async () => {};
+  const allDeleteHandler = async () => {
+    try {
+      const deleteConfirm = confirm("삭제하시겠습니까?");
+      if (deleteConfirm) {
+        // 서버에서 모든 항목 삭제시키기
+        await Promise.allSettled(
+          allTodos.map((todo) => axios.delete(`${BaseUrl}/${todo._id}`))
+        );
+
+        // 화면에서 삭제된 항목 표시
+        setAllTodos([]);
+        setImportantTodos([]);
+        setTodos([]);
+      }
+    } catch (error) {
+      console.error("항목을 전체 삭제하는데 요청 오류 발생", error);
+    }
+  };
 
   useEffect(() => {
     fetchData();
@@ -196,7 +237,7 @@ const TodoList: React.FC = () => {
         </button>
       </div>
       <div id="contents" className="todo-container">
-        <div className="todo-container_controller">
+        <div className="todo-container__controller">
           <button
             className="completeAll"
             data-done="0"
