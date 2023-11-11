567c741bd38ff4431bc15a6c71208ce65769f29b
윤태현 <yoonth0919@gmail.com>
2023-11-11 12:46:08 +0900

    Feat: css 추가 및 delete, important 함수 props 추가

--- 03.React/todo-app/src/pages/list/TodoListItem.tsx
+++ 03.React/todo-app/src/pages/list/TodoListItem.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./TodoListItem.css"
+
 export interface ItemProps {
   key: number;
   isCompleteCheck: (
@@ -8,16 +10,25 @@ export interface ItemProps {
     important: boolean,
     _id: number
   ) => void;
+  deleteItem: (
+    _id: number,
+    important: boolean
+  ) => void;
+  importantItem: (
+    btn: React.MouseEvent<HTMLButtonElement>,
+    _id: number,
+    important: boolean
+  ) => void;
   todo: TodoItem;
 }
 
-const TodoListItem: React.FC<ItemProps> = ({ key, isCompleteCheck, todo }) => {
+const TodoListItem: React.FC<ItemProps> = ({ isCompleteCheck, deleteItem, importantItem, todo }) => {
   return (
-    <li key={key} className="todo-item">
+    <li className="todo-item">
       <button
         type="button"
-        className="todo-item--important-button"
-        // onClick={handleClick}
+        className={`todo-item--important-button ${todo.important ? 'fill' : ''}`}
+        onClick={(e) => importantItem(e, todo._id, todo.important)}
       ></button>
       <input
         type="checkbox"
@@ -25,12 +36,12 @@ const TodoListItem: React.FC<ItemProps> = ({ key, isCompleteCheck, todo }) => {
         onChange={(e) => isCompleteCheck(e, todo.important, todo._id)}
         className="todo-item--checkbox"
       />
-      <Link to="/" className="todo-item--todoInfo-link">
+      <Link to={`/info?_id=${todo._id}`} className="todo-item--todoInfo-link">
         {todo.title}
       </Link>
       <button
-        className="todo-item-delete-button"
-        // onClick={handleDelete}
+        className="todo-item--delete-button"
+        onClick={() => deleteItem(todo._id, todo.important)}
       ></button>
     </li>
   );
