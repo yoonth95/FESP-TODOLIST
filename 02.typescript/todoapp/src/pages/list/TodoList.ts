5c396e07539cd723aceeb5ad0e3783459d47e946
Suam Kang <ksa199653@gmail.com>
2023-11-11 18:40:50 +0900

    fix: 타입 재설정

--- 02.typescript/todoapp/src/pages/list/TodoList.ts
+++ 02.typescript/todoapp/src/pages/list/TodoList.ts
@@ -14,14 +14,14 @@ import "./TodoList.css";
 import "../../global.css";
 
 const TodoList = async function () {
-  const page = document.createElement("div");
+  const page: HTMLDivElement = document.createElement("div");
   page.setAttribute("id", "page");
 
-  const contents = document.createElement("div");
+  const contents: HTMLDivElement = document.createElement("div");
   contents.setAttribute("id", "contents");
   contents.setAttribute("class", "todo-container");
 
-  const checkList = document.createElement("div");
+  const checkList: HTMLDivElement = document.createElement("div");
   checkList.setAttribute("class", "todo-container__controller");
 
   /* 필터버튼 */
@@ -30,27 +30,39 @@ const TodoList = async function () {
   const dataResult = await axios(`${BASE_URL}`);
   const todolistData = dataResult?.data.items;
 
-  const filterList = document.createElement("div");
+  const filterList: HTMLDivElement = document.createElement("div");
   filterList.setAttribute("class", "filter-list");
 
   // 전체보기 필터
-  const filterAll = Button("filter-list__item", "button", "전체보기", () =>
-    HandleDataAll(".todolist", todolistData)
+  const filterAll: HTMLButtonElement = Button(
+    "filter-list__item",
+    "button",
+    "전체보기",
+    () => HandleDataAll(".todolist", todolistData)
   );
 
   // 중요필터
-  const filterImportant = Button("filter-list__item", "button", "중요", () =>
-    HandleDataFilter(".todolist", todolistData, "important")
+  const filterImportant: HTMLButtonElement = Button(
+    "filter-list__item",
+    "button",
+    "중요",
+    () => HandleDataFilter(".todolist", todolistData, "important")
   );
 
   // 미완료 필터
-  const filterIncomplete = Button("filter-list__item", "button", "미완료", () =>
-    HandleDataFilter(".todolist", todolistData, "!done")
+  const filterIncomplete: HTMLButtonElement = Button(
+    "filter-list__item",
+    "button",
+    "미완료",
+    () => HandleDataFilter(".todolist", todolistData, "!done")
   );
 
   // 완료 필터
-  const filterComplete = Button("filter-list__item", "button", "완료", () =>
-    HandleDataFilter(".todolist", todolistData, "done")
+  const filterComplete: HTMLButtonElement = Button(
+    "filter-list__item",
+    "button",
+    "완료",
+    () => HandleDataFilter(".todolist", todolistData, "done")
   );
 
   filterList.append(
@@ -61,23 +73,27 @@ const TodoList = async function () {
   );
 
   // 등록버튼
-  const registButton = Button("registButton", "button", "등록");
+  const registButton: HTMLButtonElement = Button(
+    "registButton",
+    "button",
+    "등록"
+  );
 
   // active 컨테이너 -> 필터링 버튼, 등록버튼
-  const activeContainer = document.createElement("div");
+  const activeContainer: HTMLDivElement = document.createElement("div");
   activeContainer.setAttribute("class", "active-container");
 
   activeContainer.appendChild(filterList);
   activeContainer.appendChild(registButton);
 
   // 전체완료 버튼텍스트
-  const completedAll = document.createElement("button");
+  const completedAll: HTMLButtonElement = document.createElement("button");
   completedAll.setAttribute("class", "completeAll");
   completedAll.innerHTML = "✅ 전체완료";
   completedAll.setAttribute("data-done", "0");
 
   // 전체삭제 버튼텍스트
-  const deleteAll = document.createElement("button");
+  const deleteAll: HTMLButtonElement = document.createElement("button");
   deleteAll.setAttribute("class", "deleteAll");
   deleteAll.setAttribute("name", "deleteAll");
   deleteAll.innerHTML = "❌ 전체삭제";
@@ -87,23 +103,23 @@ const TodoList = async function () {
   checkList.appendChild(deleteAll);
 
   // 서버 통신 로직
-  // try {
+
   let response;
   response = await axios<TodoListResponse>(`${BASE_URL}`);
   const todosResponse = response.data?.items;
   // 중요 아이템 리스트 컨테이너
-  const importantList = document.createElement("ul");
+  const importantList: HTMLUListElement = document.createElement("ul");
   importantList.setAttribute("class", "important-list");
   // 일반 아이템 리스트 컨테이너
-  const normalList = document.createElement("ul");
+  const normalList: HTMLUListElement = document.createElement("ul");
   normalList.setAttribute("class", "todolist");
 
   // 인풋엘리먼트 체크박스를 원소로 같는 배열
   const checkboxList: checkboxTypeI[] = [];
 
-  todosResponse.forEach((item) => {
+  todosResponse.forEach((item: TodoItem) => {
     /* todoItem 초기렌더링 */
-    const li = TodoListItem(item);
+    const li: HTMLLIElement = TodoListItem(item);
     if (item.important) {
       importantList.style.display = "block";
       importantList.appendChild(li);
@@ -112,7 +128,7 @@ const TodoList = async function () {
     }
   });
 
-  const listAll = document.createElement("div");
+  const listAll: HTMLDivElement = document.createElement("div");
   listAll.setAttribute("class", "todo-list-all");
   listAll.appendChild(importantList);
   listAll.appendChild(normalList);
@@ -125,27 +141,23 @@ const TodoList = async function () {
   // 아이템 전체 완료 함수
   const handleAllDone = () => {
     /* 전체완료 체크박스 토글링 */
-    let toggleCompletAll =
-      Number(completedAll.dataset.done) === 0 ? false : true;
+    let toggleCompletAll: boolean =
+      completedAll.dataset.done === "0" ? false : true;
 
-    checkboxList.forEach((checkbox) => {
-      if (checkbox.checked !== toggleCompletAll) {
-        checkbox.checked = toggleCompletAll; // 눌려진 체크박스 인풋요소가 상태가 다르면 업데이트
-        const todoInfoLink = checkbox.nextSibling;
-        todoInfoLink.style.textDecoration = checkbox.checked
-          ? "line-through"
-          : "none";
-        // http 요청
-        todosResponse.forEach(async (item) => {
-          return await axios.patch(`${BASE_URL}/${item._id}`, {
-            done: toggleCompletAll, // true
-          });
+    checkboxList.forEach((checkbox: checkboxTypeI) => {
+      checkbox.checked = !toggleCompletAll; // 체크박스 전체 수정
+      console.log(checkbox.checked);
+      const todoInfoLink: HTMLAnchorElement = checkbox.nextSibling;
+      todoInfoLink.style.textDecoration = checkbox.checked
+        ? "line-through"
+        : "none";
+
+      // http 요청
+      todosResponse.forEach((item: TodoItem) => {
+        return axios.patch(`${BASE_URL}/${item._id}`, {
+          done: toggleCompletAll, // true
         });
-      }
-
-      toggleCompletAll = !toggleCompletAll;
-      // 전체완료 버튼 data-done 속성 바꾸기
-      completedAll.dataset.done = toggleCompletAll ? "1" : "0";
+      });
     });
   };
 
@@ -175,10 +187,6 @@ const TodoList = async function () {
   registButton.addEventListener("click", () => {
     linkTo("regist");
   });
-  // } catch (err) {
-  //   const error = document.createTextNode("일시적인 오류 발생");
-  //   console.log(err);
-  // }
 
   // UI 렌더
   page.appendChild(Header("TODOLIST"));
